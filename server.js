a5dba0ea2f89bba9ec2dd3d34f53365ab38c33d9
Saunak Surani <suranisaunak@gmail.com>
2017-11-08 11:52:00 +0530

    removed

--- server.js
+++ server.js
@@ -6,34 +6,6 @@ var wss = new WebSocketServer({port: process.env.PORT || 5000});
 
 //all connected to the server users 
 var users = {};
-
-var http = require('http'), io = require('socket.io');
-
-// Start the server at port 8080
-var server = http.createServer(function(req, res){ 
-
-    // Send HTML headers and message
-    res.writeHead(200,{ 'Content-Type': 'text/html' }); 
-    res.end('<h1>Hello Socket Lover!</h1>');
-});
-server.listen(8080);
-
-// Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
-
-// Add a connect listener
-socket.on('connection', function(client){ 
-
-    // Success!  Now listen to messages to be received
-    client.on('message',function(event){ 
-        console.log('Received message from client!',event);
-    });
-    client.on('disconnect',function(){
-        clearInterval(interval);
-        console.log('Server has disconnected');
-    });
-
-});
   
 //when a user connects to our sever 
 wss.on('connection', function(connection) {
