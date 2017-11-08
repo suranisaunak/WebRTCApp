41562856ade6afe66c18505208e6c81bc9c4c2bb
Saunak Surani <suranisaunak@gmail.com>
2017-11-08 11:31:14 +0530

    port added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var WebSocketServer = require('ws').Server; 
 
 //creating a websocket server at port 9090 
-var wss = new WebSocketServer(); 
+var wss = new WebSocketServer({port: 9090}); 
 
 //all connected to the server users 
 var users = {};
